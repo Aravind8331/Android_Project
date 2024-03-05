bee95d608aaedd871b637228eff242ce9fc67b68
s566646 <143014647+s566646@users.noreply.github.com>
2024-03-04 22:25:09 -0600

    Created Activity ,register and signin code

---
+++ app/src/main/java/com/example/goodwillapp/Signin.java
@@ -0,0 +1,27 @@
+package com.example.goodwillapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Signin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start Welcome activity
+                Intent intent = new Intent(Signin.this, Welcome.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
