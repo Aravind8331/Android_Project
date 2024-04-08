054a197a350b199f3727f646eb78400b0123f1eb
Aravind8331 <143053512+Aravind8331@users.noreply.github.com>
2024-04-07 21:29:02 -0500

    Update Welcome.java

--- app/src/main/java/com/example/goodwillapp/Welcome.java
+++ app/src/main/java/com/example/goodwillapp/Welcome.java
@@ -1,6 +1,7 @@
 package com.example.goodwillapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,32 +11,57 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.material.imageview.ShapeableImageView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Welcome extends AppCompatActivity {
 
     ImageView id_logout ;
     ShapeableImageView id_beneficiary ,id_contributor;
     LinearLayout id_layoutBene,layoutCont;
+    private FirebaseAuth mAuth;
+    CardView id_selfBeneficiar,id_selfcontributor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-
+        mAuth = FirebaseAuth.getInstance();
         id_logout = findViewById(R.id.id_logout);
         id_layoutBene = findViewById(R.id.id_layoutBene);
         id_contributor = findViewById(R.id.id_contributor);
+        id_selfBeneficiar = findViewById(R.id.id_selfBeneficiar);
+        id_selfcontributor = findViewById(R.id.id_selfcontributor);
         layoutCont = findViewById(R.id.layoutCont);
         id_beneficiary = findViewById(R.id.id_beneficiary);
-
 
         id_logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                mAuth.signOut();
                 Intent intent = new Intent(Welcome.this, Signin.class);
                 startActivity(intent);
                 finish();
             }
         });
+
+        id_selfcontributor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Welcome.this, SelcfActivity.class);
+                intent.putExtra("SELFCONTRIBUTOR","CONTRIBUTOR");
+                startActivity(intent);
+            }
+        });
+          id_selfBeneficiar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Welcome.this, SelcfActivity.class);
+                intent.putExtra("SELFCONTRIBUTOR","BENEFICIAR");
+                startActivity(intent);
+            }
+        });
+
+
         layoutCont.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -69,12 +95,12 @@ public class Welcome extends AppCompatActivity {
     }
 
     private void callBeneficiary() {
-        Intent intent = new Intent(Welcome.this, Contributor.class);
+        Intent intent = new Intent(Welcome.this, Beneficiary.class);
         startActivity(intent);
     }
 
     private void callContributors() {
-        Intent intent = new Intent(Welcome.this, Beneficiary.class);
+        Intent intent = new Intent(Welcome.this, Contributor.class);
         startActivity(intent);
     }
 }
